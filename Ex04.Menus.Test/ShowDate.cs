2c315fd30f8bf11e1f448bd8e1a269c0d131f4b1
orelkabetz <78811602+orelkabetz@users.noreply.github.com>
2022-05-18 12:11:03 +0300

    End of 1

--- Ex04.Menus.Test/ShowDate.cs
+++ Ex04.Menus.Test/ShowDate.cs
@@ -7,8 +7,7 @@ namespace Ex04.Menus.Test
     {
         void IOptionObserver.OnOptionChosen()
         {
-            Console.WriteLine("Today is: {0} , and the current date is: {1}/{2}/{3}"
-                , DateTime.Now.DayOfWeek ,DateTime.Now.Day, DateTime.Now.Month, DateTime.Now.Year);
+            Console.WriteLine("Today is: {0} , and the current date is: {1}/{2}/{3}", DateTime.Now.DayOfWeek ,DateTime.Now.Day, DateTime.Now.Month, DateTime.Now.Year);
         }
     }
 }
